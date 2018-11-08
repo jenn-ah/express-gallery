42488084891e0ec87f1509a0c04c32047c145409
JennAh <jennah@Jenns-Air.home>
2018-11-08 09:28:39 -1000

    needed to rebuild docker containers to reformat tables with proper author id

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,6 +1,33 @@
 const express = require('express');
+const User = require('../db/models/User');
+const Photo = require('../db/models/Photo');
 const router = express.Router();
 
+// router.get('/', (req, res) => {
+//   //res.send('users router smoke');
+//   return Photo.fetchAll()
+//     .then(photos => {
+//       res.json(photos);
+//     })
+//     .catch(err => console.log(err));
+// });
 
+router.post('/', (req, res) => {
+  let data = req.body;
+  data.description = data.description.toString();
+  console.log('data', data);
+  return new Photo({
+    id: data.id,
+    name: data.author,
+    link: data.link,
+    description: data.description
+  })
+    .save()
+    //persists the data to the database;
+    .then(photo => {
+      return res.json(photo);
+    })
+    .catch(err => console.log(err))
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.EXPRESS_HOST_PORT;
 const app = express();
-// const usersRouter = require('./routes/users');
-// const postsRouter = require('./routes/posts');
+const galleryRouter = require('./routes/gallery');
+
 
 app.use(bodyParser.urlencoded({ extended: true}));
-// app.use('/users', usersRouter);
-// app.use('/posts', postsRouter);
+app.use('/gallery', galleryRouter);
+
 
 app.get('/', (req, res) => {
-  res.send('smoke test');
+  return Photo.fetchAll()
+    .then(photos => {
+      res.json(photos);
+    })
+    .catch(err => console.log(err));
 });
 
 app.listen(PORT, () => {
